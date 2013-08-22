9084561899e2df28d6322e27d46ac1637aa08564
Bill Walker <bwalker@mozilla.com>
2013-08-22 12:59:52 -0700

    make new simplified version of getting data from backend server

--- server.js
+++ server.js
@@ -6,49 +6,40 @@
 var express = require('express');
 var url = require('url');
 var http = require('http');
+var https = require('https');
+var apikey = require('./apikey-real');
 
 var app = express.createServer();
 
 app.use(express.cookieParser());
 app.use(express.session({secret: 'twegrergq25y345y245y'}));
 
-// function respondWithProxiedJSON(request, response, inURL) {
-//   // parse the URL
-//   var backendURL = url.parse(inURL, backendRequest);
+function respondWithProxiedJSON(request, response, inURL) {
+  backendURL = url.parse(inURL);
+  // parse the URL
+  console.log(inURL);
 
-//   // create a client to the given port and hostname
-//   var backendClient = http.createClient(backendURL['port'] || 80, backendURL['hostname']);
+  var options = {
+    host: backendURL.hostname,
+    headers: {accept: 'application/json, text/plain, */*'},
+    path: backendURL.path,
+    method: 'GET'
+  };
 
-//   // create a request to that client using method, path; supply host header with hostname
-//   var backendRequest = backendClient.request(backendURL['method'] || 'GET', backendURL['pathname'], {'host': backendURL['hostname']});
+  console.log("options");
+  console.log(options);
 
-//   // add a listener to the request...
-//   backendRequest.addListener('response', function (backendResponse) {
+  https.get(options, function(backendResponse) {
+    console.log("statusCode: ", backendResponse.statusCode);
+    console.log("headers: ", backendResponse.headers);   
 
-//     // ...so that when the backend responds with a chunk, we write it to the frontend response
-//     backendResponse.addListener('data', function(chunk) {
-//       response.write(chunk, 'binary');
-//     });
+    backendResponse.on('data', function(data) {
+      console.log('data ' + data);
+      response.send(data);
+    });
+  });
 
-//     // ...and that when the backend closes, the frontend closes
-//     backendResponse.addListener('end', function() {
-//       response.end();
-//     });
-
-//     // kick it off by writing the head to the frontend response as soon as we get one from the backend response
-//     response.writeHead(backendResponse.statusCode, backendResponse.headers);
-//   });
-
-//   // just in case there's a request payload, wire up so that frontend data chunks are sent to the backend
-//   request.addListener('data', function(chunk) {
-//     backendRequest.write(chunk, 'binary');
-//   });
-
-//   // just in case there's a request payload, wire up so that frontend request ENDs results in backend request END
-//   request.addListener('end', function() {
-//     backendRequest.end();
-//   });
-// };
+};
 
 app.use("/client", express.static('client'));
 
@@ -68,9 +59,12 @@ app.get('/mozillians', function(request, response) {
 	response.send(JSON.stringify(fakeResponse));
 });
 
-// app.get('/commits/:user/:project', function(request, response) {
-//   respondWithProxiedJSON(request, response, 'http://github.com/api/v2/json/commits/list/' + request.params.user +'/' + request.params.project + '/master');
-// });
+app.get('/realmozillians', function(request, response) {
+	var searchStem = 'https://mozillians.org/api/v1/users/?&limit=500&format=json&app_name=' + apikey.getAPIAppName() + '&app_key=' + apikey.getAPIKey();
+	var searchURL = searchStem + '&city=brighton';
+  console.log(searchURL);
+	respondWithProxiedJSON(request, response, searchURL);
+});
 
 console.log("running");
 
